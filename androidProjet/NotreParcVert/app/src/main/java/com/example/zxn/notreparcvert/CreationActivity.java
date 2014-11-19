c9c4e6651a8656d04df490a1878eaa420695cf5e
jamescheng16 <jamescheng16@gmail.com>
2014-11-20 00:08:35 +0100

    create

--- androidProjet/NotreParcVert/app/src/main/java/com/example/zxn/notreparcvert/CreationActivity.java
+++ androidProjet/NotreParcVert/app/src/main/java/com/example/zxn/notreparcvert/CreationActivity.java
@@ -1,17 +1,29 @@
 package com.example.zxn.notreparcvert;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class CreationActivity extends ActionBarActivity {
+
+    private Probleme probleme;
+    private EditText et_lat,et_lon,loca_exa,des;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
+
+       et_lat =  (EditText) findViewById(R.id.et_lat);
+       et_lon =  (EditText) findViewById(R.id.et_lon);
+        loca_exa =  (EditText) findViewById(R.id.loca_exa);
+        des =  (EditText) findViewById(R.id.des);
+
+
     }
 
 
@@ -32,5 +44,9 @@ public class CreationActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void save(View view){
+
     }
 }
