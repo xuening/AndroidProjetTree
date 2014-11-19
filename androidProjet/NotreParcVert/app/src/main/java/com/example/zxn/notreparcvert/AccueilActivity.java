3c19c17e942b0204ce29be9a850279fd4f6a47f0
xuening <zhaoxuening7@gmail.com>
2014-11-19 23:31:53 +0100

    change to creation

--- androidProjet/NotreParcVert/app/src/main/java/com/example/zxn/notreparcvert/AccueilActivity.java
+++ androidProjet/NotreParcVert/app/src/main/java/com/example/zxn/notreparcvert/AccueilActivity.java
@@ -12,25 +12,19 @@ public class AccueilActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-    }
 
+        // le button
+        Button btn_cre = (Button)findViewById(R.id.btn_cre);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.accueil, menu);
-        return true;
-    }
+        // button click event
+        btn_cre.setOnClickListener(new OnClickListener() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(AccueilActivity.this, CreationActivity.class);
+                startActivity(intent);
+                finish();// stop the activity current
+            }
     }
 }
