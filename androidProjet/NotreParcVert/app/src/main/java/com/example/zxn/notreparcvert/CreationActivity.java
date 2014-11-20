c04fcd2457c6711eb3bf375cf036ebf849627d11
xuening <zhaoxuening7@gmail.com>
2014-11-20 01:18:45 +0100

    spinner type

--- androidProjet/NotreParcVert/app/src/main/java/com/example/zxn/notreparcvert/CreationActivity.java
+++ androidProjet/NotreParcVert/app/src/main/java/com/example/zxn/notreparcvert/CreationActivity.java
@@ -6,16 +6,21 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class CreationActivity extends ActionBarActivity {
 
     private Probleme probleme;
     private EditText et_lat, et_lon, loca_exa, des;
-    private Spinner spinner1;
+    private Spinner spinner_type;
     private BD bd;
+    private ArrayAdapter adapter2;
+    private TextView tv_type;
 
 
     @Override
@@ -28,7 +33,29 @@ public class CreationActivity extends ActionBarActivity {
         loca_exa = (EditText) findViewById(R.id.loca_exa);
         des = (EditText) findViewById(R.id.des);
 
+        // spinner
+        spinner_type = (Spinner)findViewById(R.id.spinner_type);
+        adapter2 = ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
+        //add adapter2 to spinner
+        spinner_type.setAdapter(adapter2);
+        //add event listener
+        spinner_type.setOnItemSelectedListener(new SpinnerXMLSelectedListener());
 
+        //set the value init
+        spinner_type.setVisibility(View.VISIBLE);
+
+        /*class SpinnerXMLSelectedListener implements AdapterView.OnItemSelectedListener {
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                       long arg3) {
+                tv_type.setText("Type：" + adapter2.getItem(arg2));
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+
+        }*/
+        // display the location of problem as soon as turned into this page
         LocationManager locMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location loc = locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         if (loc != null) {
@@ -41,19 +68,15 @@ public class CreationActivity extends ActionBarActivity {
 
 
     }
-
+    // event for creer button
+    // save the new problem to the BD
     public void save(View view) {
 
-        /*
-        personne.setNom(nom.getText().toString());
-        personne.setPrenom(prenom.getText().toString());
-        personne.setAge(Integer.parseInt(age.getText().toString()));*/
-
-
+        // get the value of all items
         probleme.setDescription(des.getText().toString());
         probleme.setLatitude(Float.parseFloat(et_lat.getText().toString()));
         probleme.setLongitude(Float.parseFloat(et_lon.getText().toString()));
-        probleme.setType(spinner1.getSelectedItem().toString());
+        probleme.setType(spinner_type.getSelectedItem().toString());
         probleme.setLoc_exacte(loca_exa.getText().toString());
 
         bd.ajouter(probleme);
